e5090c3d6d2c501bee696e25033ca19d6ca36c9c
Mehtab Ahmed <95327561+Mehtab66@users.noreply.github.com>
2025-04-12 11:34:26 +0500

    even oddd prog using bit operator

---
+++ NumberSystems/CheckEvenOdd.java
@@ -0,0 +1,19 @@
+package NumberSystems;
+
+import java.util.*;
+
+public class CheckEvenOdd {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int num = scanner.nextInt();
+        if ((num & 1) == 1) {
+            System.out.print("The number is odd");
+        } else {
+            System.out.print("The number is even");
+        }
+
+    }
+
+}
